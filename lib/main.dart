54dd2a9279b914974599497ba841b86351c9aa46
lhmisho <lhmisho@gmail.com>
2020-10-23 18:23:22 +0600

    add interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -37,15 +37,15 @@ class _RandomWordState extends State<RandomWord> {
   Widget _buildSuggestion() {
     return ListView.builder(
         padding: EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return Divider(); /*2*/
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
 
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return _buildRow(_suggestions[index]);
-      });
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
 
   Widget _buildRow(WordPair pair) {
@@ -59,6 +59,16 @@ class _RandomWordState extends State<RandomWord> {
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+      
     );
   }
 }
